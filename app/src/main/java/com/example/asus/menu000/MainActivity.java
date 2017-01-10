61e2c7e12fc135ca1c8b90b102bfc6fc78fe9a34
yz <1255125169@qq.com>
2017-01-10 11:49:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/asus/menu000/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.asus.menu000;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+//签名
+public class MainActivity extends AppCompatActivity {
+    private TextView tv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv1=(TextView)findViewById(R.id.textView);
+        registerForContextMenu(tv1);
+    }
+    //创建选项菜单
+    public boolean onCreateOptionsMenu(Menu menu){
+        menu.add(0,1,0,R.string.m1);
+        menu.add(0,1,0,R.string.m2);
+        return super.onCreateOptionsMenu(menu);
+    }
+    //选择菜单被选中
+    public boolean onOptionsItemSelected(MenuItem item){
+        Toast.makeText(this,item.getTitle().toString(),Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+    }
+
+    //创建上下文菜单
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0,1,0,R.string.m1);
+        menu.add(0,1,0,R.string.m2);
+    }
+
+    //上下文菜单被选中
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Toast.makeText(this,item.getTitle(),Toast.LENGTH_LONG).show();
+        return super.onContextItemSelected(item);
+    }
+}
